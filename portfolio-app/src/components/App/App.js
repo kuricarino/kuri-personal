4087633efc1a9581d4f6aac27ba39cd48331ec37
Kristine Ilano <kuri.carino@Kristines-MacBook-Pro.local>
2020-08-05 15:37:17 -0700

    Work on Header, added toggle effect

---
+++ portfolio-app/src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from '../../layout/Header/Header';
+// import Footer from '../../layout/Footer/Footer';
+
+function App() {
+  return (
+    <section class="hero is-primary is-medium">
+      <Header />
+      {/* <Routes />
+      <Footer /> */}
+    </section>
+  );
+}
+
+export default App;
